14d634d5b78f684a3fbcbbc398b1abc7cbadb707
unknown <shyam@rivelearning.in>
2022-04-19 16:52:30 +0530

    All section templates

--- frontend/views.py
+++ frontend/views.py
@@ -43,3 +43,93 @@ def developmentGoals(request):
 def myTrainings(request):
     return render(request, 'frontend/my-trainings.html')
 
+def createTask(request):
+    return render(request, 'frontend/create-task.html')
+
+def managerEvaluation(request):
+    return render(request, 'frontend/manager-evaluation.html')
+
+def ownerEvaluation(request):
+    return render(request, 'frontend/owner-evaluation.html')
+
+def salaryReport(request):
+    return render(request, 'frontend/salary-report.html')
+
+def internalProjects(request):
+    return render(request, 'frontend/internal-projects.html')
+
+def strategicGoals(request):
+    return render(request, 'frontend/strategic-goals.html')
+
+def mission(request):
+    return render(request, 'frontend/mission.html')
+
+def organizationChart(request):
+    return render(request, 'frontend/organization-chart.html')
+
+def createStrategy(request):
+    return render(request, 'frontend/create-strategy.html')
+
+def createInternalProject(request):
+    return render(request, 'frontend/create-internal-project.html')
+
+def employees(request):
+    return render(request, 'frontend/employees.html')
+
+def departments(request):
+    return render(request, 'frontend/departments.html')
+
+def createNewDepartment(request):
+    return render(request, 'frontend/create-new-department.html')
+
+def roles(request):
+    return render(request, 'frontend/roles.html')
+
+def createNewRole(request):
+    return render(request, 'frontend/create-new-role.html')
+
+def jobRequirementsKRA(request):
+    return render(request, 'frontend/job-requirements-kra.html')
+
+def createNewJobRequirement(request):
+    return render(request, 'frontend/create-new-job-requirement.html')
+
+def grades(request):
+    return render(request, 'frontend/grades.html')
+
+def createNewGrade(request):
+    return render(request, 'frontend/create-new-grade.html')   
+
+def appraisalCycle(request):
+    return render(request, 'frontend/appraisal-cycle.html')
+
+def createNewAppraisalCycle(request):
+    return render(request, 'frontend/create-new-appraisal-cycle.html')
+
+def kpi(request):
+    return render(request, 'frontend/kpi.html')
+
+def createNewKPI(request):
+    return render(request, 'frontend/create-new-kpi.html')    
+
+def trainingAdmin(request):
+    return render(request, 'frontend/training-admin.html') 
+
+def createNewTraining(request):
+    return render(request, 'frontend/create-new-training.html') 
+
+def trainers(request):
+    return render(request, 'frontend/trainers.html') 
+
+def createNewTrainer(request):
+    return render(request, 'frontend/create-new-trainer.html')
+
+def topics(request):
+    return render(request, 'frontend/topics.html')
+
+def createNewTopic(request):
+    return render(request, 'frontend/create-new-topic.html')
+
+def personWiseReport(request):
+    return render(request, 'frontend/person-wise-report.html')
+
