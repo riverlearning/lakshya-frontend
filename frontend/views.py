fb0cbbde22cb59a22dfdcb9f08a820aefa78a26d
shyam0rathod <shyam@riverlearning.in>
2022-06-01 13:16:59 +0530

    remaining changes

--- frontend/views.py
+++ frontend/views.py
@@ -133,3 +133,69 @@ def createNewTopic(request):
 def personWiseReport(request):
     return render(request, 'frontend/person-wise-report.html')
 
+def roleJRMapping(request):
+    return render(request, 'frontend/role-jr-mapping.html')
+
+def employeeJRMapping(request):
+    return render(request, 'frontend/employee-jr-mapping.html')
+
+def monthlyTrainingReport(request):
+    return render(request, 'frontend/monthly-training-report.html')
+
+def editMyTasks(request):
+    return render(request, 'frontend/edit-my-tasks.html')
+
+def addComment(request):
+    return render(request, 'frontend/add-comment.html')
+
+def editTasksAllocated(request):
+    return render(request, 'frontend/edit-tasks-allocated.html')
+
+def assessClosedTasks(request):
+    return render(request, 'frontend/assess-closed-tasks.html')
+
+def managerEvaluateNow(request):
+    return render(request, 'frontend/manager-evaluate-now.html')
+
+def ownerEvaluateNow(request):
+    return render(request, 'frontend/owner-evaluate-now.html')
+
+def updateEmployee(request):
+    return render(request, 'frontend/update-employee.html')
+
+def updateDepartment(request):
+    return render(request, 'frontend/update-department.html')
+
+def updateRole(request):
+    return render(request, 'frontend/update-role.html')
+
+def updateJobRequirement(request):
+    return render(request, 'frontend/update-job-requirement.html')
+
+def updateGrade(request):
+    return render(request, 'frontend/update-grade.html')
+
+def updateAppraisalCycle(request):
+    return render(request, 'frontend/update-appraisal-cycle.html')
+
+def appraisalReport(request):
+    return render(request, 'frontend/appraisal-report.html')
+
+def taskDetails(request):
+    return render(request, 'frontend/task-details.html')
+
+def updateKPI(request):
+    return render(request, 'frontend/update-kpi.html')
+
+def kpiData(request):
+    return render(request, 'frontend/kpi-data.html')
+
+def appraisalDetails(request):
+    return render(request, 'frontend/appraisal-details.html')
+
+def editJobRequirement(request):
+    return render(request, 'frontend/edit-job-requirement.html')
+
+def addJobRequirement(request):
+    return render(request, 'frontend/add-job-requirement.html')
+
